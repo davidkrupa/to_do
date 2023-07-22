480a0affb3754f51aa68daa2021caea150393a6e
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-07-22 19:47:05 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="app__wrapper">
+      <div>New note body</div>
+      <div>saved notes</div>
+    </div>
+  );
+}
+
+export default App;
